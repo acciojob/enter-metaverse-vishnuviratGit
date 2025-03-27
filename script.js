450f55951659d0c50aeda2128285d7e99eaa5e24
vishnuviratGit <vishnuvirat564@gmail.com>
2025-03-27 13:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", function(){
+	document.getElementById("status").innerHTML="<h1>Entered the Metaverse</h1>";
+})
